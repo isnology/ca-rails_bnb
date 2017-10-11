3b40639e92aeb9084d7f090875804024e9161153
IsNology <glenn@isnology.com>
2017-10-11 14:22:24 +1100

    add money gem

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'annotate'
 gem 'devise'
 gem 'countries'
 gem 'geocoder'
+gem 'money'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -54,3 +55,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+ruby "2.4.1"
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  street_address     :string
+#  city               :string
+#  country_code       :string
+#  bed_count          :integer
+#  bedroom_count      :integer
+#  bathroom_count     :integer
+#  description        :text
+#  night_fee_cents    :integer
+#  cleaning_fee_cents :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  host_id            :integer
+#  latitude           :decimal(10, 6)
+#  longitude          :decimal(10, 6)
+#
+require 'money'
+
 class Listing < ApplicationRecord
   geocoded_by :full_address   # can also be an IP address
   after_validation :geocode, if: ->(obj){ obj.location.present? and obj.location_changed? } # auto-fetch coordinates
@@ -11,5 +34,11 @@ class Listing < ApplicationRecord
     "#{street_address}, #{city}, #{country.name}"
   end
   
-  
+  def night_fee
+    Money.new(night_fee_cents, 'AUD')
+  end
+
+  def cleaning_fee
+    Money.new(cleaning_fee_cents, 'AUD')
+  end
 end
